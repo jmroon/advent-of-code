0bf43618c65017b7315f38ab28a312905b10db79
jmroon <jmroon@gmail.com>
2022-12-05 00:42:23 -0500

    added data message

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import * as fs from "fs/promises";
 const script = await import(`./${process.argv[2]}.js`);
 const data = (await fs.readFile(`./data/${process.argv[2]}.txt`)).toString();
 
+if (data.length === 0) console.error(`No data found for ${process.argv[2]}`);
+
 console.log(`Part 1: ${script.part1(data)}`);
 console.log(`Part 2: ${script.part2(data)}`);
 
