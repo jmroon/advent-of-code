2d0d857df6947481c135c4115e5b79bba27e63ad
jmroon <jmroon@gmail.com>
2022-12-05 00:36:22 -0500

    added blank days

--- src/2022/1.ts
+++ src/2022/1.ts
@@ -1,29 +1,7 @@
 export function part1(input: string) {
-  return caloriesByElfDescending(input)[0];
+  return "not implemented";
 }
 
 export function part2(input: string) {
-  const caloriedByElfDescending = caloriesByElfDescending(input);
-  return (
-    caloriedByElfDescending[0] +
-    caloriedByElfDescending[1] +
-    caloriedByElfDescending[2]
-  );
-}
-
-function caloriesByElfDescending(input: string): number[] {
-  // get an array of numbers for each elf
-  const calorieListByElf = input
-    .split("\n\n")
-    .map((elf) => elf.split("\n").map((item) => parseInt(item)));
-
-  // reduce the array of calories to get the total calories for each elf
-  const caloriesByElf = calorieListByElf.map((calorieListByElf) =>
-    calorieListByElf.reduce((total, calories) => (total += calories), 0)
-  );
-
-  // sort caloriesByElf in descending order
-  caloriesByElf.sort((a, b) => b - a);
-
-  return caloriesByElf;
+  return "not implemented";
 }
