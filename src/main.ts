7a5a9b486f7961808040118e94109043ca43fa3e
jmroon <jmroon@gmail.com>
2022-12-05 13:41:38 -0500

    normalize line endings

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import * as fs from "fs/promises";
 
 const script = await import(`./${process.argv[2]}.js`);
-const data = (await fs.readFile(`./data/${process.argv[2]}.txt`)).toString();
+const data = (await fs.readFile(`./data/${process.argv[2]}.txt`))
+  .toString()
+  .replaceAll("\r\n", "\n");
 
 if (data.length === 0) console.error(`No data found for ${process.argv[2]}`);
 
