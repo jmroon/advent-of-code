08f84f38490373174beaa6ccae315c97bdbb7ffc
jmroon <jmroon@gmail.com>
2022-12-06 16:20:30 -0500

    day 5

--- src/2022/5.ts
+++ src/2022/5.ts
@@ -1,7 +1,70 @@
 export function part1(input: string) {
-  return "not implemented";
+  const stacks = createStacks(input);
+  const moves = parseMoves(input);
+
+  moves.forEach((move) => {
+    const cargo = stacks[move.from - 1]
+      .splice(stacks[move.from - 1].length - move.count, move.count)
+      .reverse();
+    stacks[move.to - 1].push(...cargo);
+  });
+
+  return stacks.reduce((result, stack) => result + stack[stack.length - 1], '');
 }
 
 export function part2(input: string) {
-  return "not implemented";
+  const stacks = createStacks(input);
+  const moves = parseMoves(input);
+
+  moves.forEach((move) => {
+    const cargo = stacks[move.from - 1].splice(
+      stacks[move.from - 1].length - move.count,
+      move.count
+    );
+    stacks[move.to - 1].push(...cargo);
+  });
+
+  return stacks.reduce((result, stack) => result + stack[stack.length - 1], '');
+}
+
+export function createStacks(input: string) {
+  const stackNumbers = input
+    .match(/^[\d|\s]*\n$/gm)![0]
+    .trim()
+    .replaceAll(/\s+/g, ' ')
+    .split(' ')
+    .map((num) => parseInt(num));
+
+  const stacklines = input.match(/.*(?=\n\s\d)/gs)![0].split('\n');
+
+  return stackNumbers.map((num) =>
+    stacklines
+      .reduce<string[]>((stack, line) => {
+        const cargo = line.charAt((num - 1) * 4 + 1);
+        if (cargo !== ' ') {
+          stack.push(cargo);
+        }
+        return stack;
+      }, [])
+      .reverse()
+  );
+}
+
+export function parseMoves(input: string): StackMove[] {
+  return input.split('\n').reduce<StackMove[]>((moves, line) => {
+    const move = line.match(/move (\d+) from (\d+) to (\d+)/);
+    if (move) {
+      moves.push({
+        count: parseInt(move[1]),
+        from: parseInt(move[2]),
+        to: parseInt(move[3]),
+      });
+    }
+    return moves;
+  }, []);
+}
+export interface StackMove {
+  count: number;
+  from: number;
+  to: number;
 }
--- test/5.test.ts
+++ test/5.test.ts
@@ -1,6 +1,15 @@
-import { part1, part2 } from '../src/2022/5';
+import { StackMove } from './../src/2022/5';
+import { part1, part2, createStacks, parseMoves } from '../src/2022/5';
 
-const data = ``;
+const data = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`;
 
 describe('Day 5 Tests', () => {
   it('should exist', () => {
@@ -8,11 +17,26 @@ describe('Day 5 Tests', () => {
     expect(part2).toBeDefined();
   });
 
-  // it('should solve part 1', () => {
-  //   expect(part1(data)).toBe();
-  // });
+  it('should solve part 1', () => {
+    expect(part1(data)).toBe('CMZ');
+  });
 
-  // it('should solve part 2', () => {
-  //   expect(part2(data)).toBe();
-  // });
+  it('should create stacks from input', () => {
+    expect(createStacks(data)).toEqual([['Z', 'N'], ['M', 'C', 'D'], ['P']]);
+  });
+
+  it('should parse moves', () => {
+    const expectedMoves: StackMove[] = [
+      { count: 1, from: 2, to: 1 },
+      { count: 3, from: 1, to: 3 },
+      { count: 2, from: 2, to: 1 },
+      { count: 1, from: 1, to: 2 },
+    ];
+
+    expect(parseMoves(data)).toEqual(expectedMoves);
+  });
+
+  it('should solve part 2', () => {
+    expect(part2(data)).toBe('MCD');
+  });
 });
