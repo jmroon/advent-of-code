9bda0ded38f3843c66d8f71c39412fb6d519ce39
jmroon <jmroon@gmail.com>
2022-12-06 00:19:31 -0500

    day 4

--- src/2022/4.ts
+++ src/2022/4.ts
@@ -1,7 +1,54 @@
 export function part1(input: string) {
-  return "not implemented";
+  const lines = input.split('\n');
+  const ranges = lines.map((line): { elf1Range: Range; elf2Range: Range } => {
+    const [elf1, elf2] = line.split(',');
+    const [elf1Start, elf1End] = elf1.split('-').map((num) => parseInt(num));
+    const [elf2Start, elf2End] = elf2.split('-').map((num) => parseInt(num));
+    return {
+      elf1Range: { start: elf1Start, end: elf1End },
+      elf2Range: { start: elf2Start, end: elf2End },
+    };
+  });
+  return ranges.reduce((acc, range) => {
+    if (
+      (range.elf1Range.start >= range.elf2Range.start &&
+        range.elf1Range.end <= range.elf2Range.end) ||
+      (range.elf2Range.start >= range.elf1Range.start &&
+        range.elf2Range.end <= range.elf1Range.end)
+    ) {
+      acc += 1;
+    }
+    return acc;
+  }, 0);
 }
 
 export function part2(input: string) {
-  return "not implemented";
+  const lines = input.split('\n');
+  const ranges = lines.map((line): { elf1Range: Range; elf2Range: Range } => {
+    const [elf1, elf2] = line.split(',');
+    const [elf1Start, elf1End] = elf1.split('-').map((num) => parseInt(num));
+    const [elf2Start, elf2End] = elf2.split('-').map((num) => parseInt(num));
+    return {
+      elf1Range: { start: elf1Start, end: elf1End },
+      elf2Range: { start: elf2Start, end: elf2End },
+    };
+  });
+  return ranges.reduce((acc, range) => {
+    let elf1Sections: number[] = [];
+    let elf2Sections: number[] = [];
+    for (let i = range.elf1Range.start; i <= range.elf1Range.end; i++) {
+      elf1Sections.push(i);
+    }
+    for (let i = range.elf2Range.start; i <= range.elf2Range.end; i++) {
+      elf2Sections.push(i);
+    }
+    return elf1Sections.find((num) => elf2Sections.includes(num))
+      ? (acc += 1)
+      : acc;
+  }, 0);
+}
+
+interface Range {
+  start: number;
+  end: number;
 }
--- test/4.test.ts
+++ test/4.test.ts
@@ -1,6 +1,11 @@
 import { part1, part2 } from '../src/2022/4';
 
-const data = ``;
+const data = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`;
 
 describe('Day 4 Tests', () => {
   it('should exist', () => {
@@ -8,11 +13,11 @@ describe('Day 4 Tests', () => {
     expect(part2).toBeDefined();
   });
 
-  // it('should solve part 1', () => {
-  //   expect(part1(data)).toBe();
-  // });
+  it('should solve part 1', () => {
+    expect(part1(data)).toBe(2);
+  });
 
-  // it('should solve part 2', () => {
-  //   expect(part2(data)).toBe();
-  // });
+  it('should solve part 2', () => {
+    expect(part2(data)).toBe(4);
+  });
 });
