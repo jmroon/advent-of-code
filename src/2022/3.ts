3e0cd09636f03d175f865cd6251a4040393cbc10
jmroon <jmroon@gmail.com>
2022-12-05 23:39:05 -0500

    day 3

--- src/2022/3.ts
+++ src/2022/3.ts
@@ -1,7 +1,63 @@
 export function part1(input: string) {
-  return 'not implemented';
+  const lines = input.split('\n');
+  const characterInBoth = lines.map((line) =>
+    getCommonCharacter(splitInHalf(line))
+  );
+  return characterInBoth.reduce(
+    (score, char) => (score += getLetterScore(char)),
+    0
+  );
 }
 
 export function part2(input: string) {
-  return 'not implemented';
+  const groups = getElfGroups(input);
+  const commonCharacters = groups.map((group) => getCommonCharacter(group));
+  return commonCharacters.reduce(
+    (score, char) => (score += getLetterScore(char)),
+    0
+  );
 }
+
+export const getElfGroups = (input: string): [string, string, string][] => {
+  const regexp = /.*\n.*\n.*\n?/g;
+  const groups = input.match(regexp)!.map((group): [string, string, string] => {
+    const [elf1, elf2, elf3] = group.split('\n');
+    return [elf1, elf2, elf3];
+  });
+  return groups;
+};
+
+export const getLetterScore = (letter: string) => {
+  if (isLowercaseLetter(letter)) {
+    return letter.charCodeAt(0) - 96;
+  } else {
+    return letter.charCodeAt(0) - 64 + 26;
+  }
+};
+
+export const isLowercaseLetter = (char: string) =>
+  char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122;
+
+export const splitInHalf = (str: string): [string, string] => {
+  const half = Math.floor(str.length / 2);
+  return [str.slice(0, half), str.slice(half)];
+};
+
+export const getCommonCharacter = ([
+  firstRucksack,
+  secondRucksack,
+  thirdRucksack,
+]: [string, string, string?]) => {
+  const firstRucksackChars = firstRucksack.split('');
+  const secondRucksackChars = secondRucksack.split('');
+
+  // this will keep it working for 2 rucksacks even if the third one is undefined
+  const thirdRucksackChars = thirdRucksack?.split('') ?? firstRucksackChars;
+
+  const commonToAll = firstRucksackChars.filter(
+    (char) =>
+      secondRucksackChars.includes(char) && thirdRucksackChars.includes(char)
+  );
+
+  return commonToAll[0];
+};
--- test/3.test.ts
+++ test/3.test.ts
@@ -1,4 +1,11 @@
-import { part1, part2 } from '../src/2022/3';
+import {
+  getLetterScore,
+  getCommonCharacter,
+  part1,
+  part2,
+  splitInHalf,
+  getElfGroups,
+} from './../src/2022/3';
 
 const data = `vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
@@ -12,7 +19,35 @@ describe('Day 3 Tests', () => {
     expect(part1(data)).toBe(157);
   });
 
-  // it('should solve part 2', () => {
-  //   expect(part2(data)).toBe(12);
-  // });
+  it('should split in half', () => {
+    expect(splitInHalf('abcdefgh')).toEqual(['abcd', 'efgh']);
+  });
+
+  it('should should get letter score', () => {
+    expect(getLetterScore('a')).toBe(1);
+    expect(getLetterScore('A')).toBe(27);
+  });
+
+  it('should get the character in both rucksacks', () => {
+    expect(getCommonCharacter(['abcd', 'efga'])).toBe('a');
+  });
+
+  it('should solve part 2', () => {
+    expect(part2(data)).toBe(70);
+  });
+
+  it('should split elves into groups', () => {
+    expect(getElfGroups(data)).toEqual([
+      [
+        'vJrwpWtwJgWrhcsFMMfFFhFp',
+        'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
+        'PmmdzqPrVvPwwTWBwg',
+      ],
+      [
+        'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
+        'ttgJtRGJQctTZtZT',
+        'CrZsJsPPZsGzwwsLwLmpwMDw',
+      ],
+    ]);
+  });
 });
