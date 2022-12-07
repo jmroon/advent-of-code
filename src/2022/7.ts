08730ccc32e0768cf655c1afe1973e8ae29d63d3
jmroon <jmroon@gmail.com>
2022-12-07 01:14:02 -0500

    day 7

--- src/2022/7.ts
+++ src/2022/7.ts
@@ -1,7 +1,90 @@
 export function part1(input: string) {
-  return "not implemented";
+  const [, flatDirs] = buildFileSystem(input.split('\n'));
+
+  return [...flatDirs.values()]
+    .filter((dir) => dir.size < 100000)
+    .reduce((totalSize, dir) => (totalSize += dir.size), 0);
 }
 
 export function part2(input: string) {
-  return "not implemented";
+  const [fileSystem, flatDirs] = buildFileSystem(input.split('\n'));
+
+  const spaceToFree = 30000000 - 70000000 + fileSystem.size;
+
+  return [...flatDirs.values()]
+    .filter((dir) => dir.size > spaceToFree)
+    .map((dir) => dir.size)
+    .sort()[0];
+}
+
+function buildFileSystem(
+  cmds: string[]
+): [fileSystem: Directory, flatDirs: Set<Directory>] {
+  const fileSystem = new Directory('/');
+
+  const flatDirs = new Set<Directory>();
+
+  let currentDir = fileSystem;
+
+  for (let i = 0; i < cmds.length; i++) {
+    const cmd = cmds[i];
+    if (cmd.startsWith('$ cd /')) {
+      currentDir = fileSystem;
+    } else if (cmd.startsWith('$ cd ')) {
+      currentDir = currentDir.cd(cmd.slice(5));
+    } else if (cmd.startsWith('$ ls ')) {
+      break;
+    } else if (cmd.startsWith('dir ')) {
+      const name = cmd.slice(4);
+      const newDir = currentDir.addDir(name);
+      if (newDir) {
+        flatDirs.add(currentDir.cd(name));
+      }
+    } else if (cmd[0].match(/\d/)) {
+      const [size, name] = cmd.split(' ');
+      currentDir.addFile(name, parseInt(size));
+    }
+  }
+  return [fileSystem, flatDirs];
+}
+
+class Directory {
+  children: Directory[] = [];
+  files: File[] = [];
+
+  constructor(public name: string, public parent?: Directory) {}
+
+  addDir(name: string): Directory | undefined {
+    if (this.children.find((dir) => dir.name === name)) {
+      return;
+    }
+    const dir = new Directory(name, this);
+    this.children.push(dir);
+    return dir;
+  }
+
+  addFile(name: string, size: number): void {
+    if (this.files.some((file) => file.name === name)) {
+      return;
+    }
+    this.files.push(new File(name, size));
+  }
+
+  get size(): number {
+    return (
+      this.files.reduce((size, file) => size + file.size, 0) +
+      this.children.reduce((dirSizes, dir) => dirSizes + dir.size, 0)
+    );
+  }
+
+  cd(name: string): Directory {
+    if (name === '..') {
+      return this.parent!;
+    }
+    return this.children.find((dir) => dir.name === name)!;
+  }
+}
+
+class File {
+  constructor(public name: string, public size: number) {}
 }
--- test/7.test.ts
+++ test/7.test.ts
@@ -1,6 +1,28 @@
 import { part1, part2 } from '../src/2022/7';
 
-const data = ``;
+const data = `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`;
 
 describe('Day 7 Tests', () => {
   it('should exist', () => {
@@ -8,11 +30,11 @@ describe('Day 7 Tests', () => {
     expect(part2).toBeDefined();
   });
 
-  // it('should solve part 1', () => {
-  //   expect(part1(data)).toBe();
-  // });
+  it('should solve part 1', () => {
+    expect(part1(data)).toBe(95437);
+  });
 
-  // it('should solve part 2', () => {
-  //   expect(part2(data)).toBe();
-  // });
+  it('should solve part 2', () => {
+    expect(part2(data)).toBe(24933642);
+  });
 });
