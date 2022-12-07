61c31479c7f234ec5c7964794214348f8ca44f56
jmroon <jmroon@gmail.com>
2022-12-06 20:31:14 -0500

    day 6

--- src/2022/6.ts
+++ src/2022/6.ts
@@ -1,7 +1,10 @@
-export function part1(input: string) {
-  return "not implemented";
-}
+export const part1 = (input: string) => getMarkerPosition(input, 4);
 
-export function part2(input: string) {
-  return "not implemented";
+export const part2 = (input: string) => getMarkerPosition(input, 14);
+
+function getMarkerPosition(input: string, markerLength: number) {
+  for (let i = 0; i < input.length - markerLength; i++) {
+    const set = new Set(input.slice(i, i + markerLength));
+    if (set.size === markerLength) return i + markerLength;
+  }
 }
--- test/6.test.ts
+++ test/6.test.ts
@@ -1,6 +1,6 @@
 import { part1, part2 } from '../src/2022/6';
 
-const data = ``;
+const data = `mjqjpqmgbljsphdztnvjfqwrcgsmlb`;
 
 describe('Day 6 Tests', () => {
   it('should exist', () => {
@@ -8,11 +8,11 @@ describe('Day 6 Tests', () => {
     expect(part2).toBeDefined();
   });
 
-  // it('should solve part 1', () => {
-  //   expect(part1(data)).toBe();
-  // });
+  it('should solve part 1', () => {
+    expect(part1(data)).toBe(7);
+  });
 
-  // it('should solve part 2', () => {
-  //   expect(part2(data)).toBe();
-  // });
+  it('should solve part 2', () => {
+    expect(part2(data)).toBe(19);
+  });
 });
