e44a0e876dbfd97e32542b80f851188090322159
jmroon <jmroon@gmail.com>
2022-12-13 11:41:05 -0500

    day 13

--- src/2022/13.ts
+++ src/2022/13.ts
@@ -1,7 +1,58 @@
 export function part1(input: string) {
-  return "not implemented";
+  const pairs = input
+    .split('\n\n')
+    .map((pair) => pair.split('\n').map((packet) => JSON.parse(packet)) as [Packet, Packet]);
+
+  const counts = pairs.reduce((counts, pair, index) => {
+    return compare(pair[0], pair[1]) === -1 ? [...counts, index + 1] : counts;
+  }, [] as number[]);
+
+  console.log(counts);
+
+  return counts.reduce((count, value) => count + value, 0);
 }
 
 export function part2(input: string) {
-  return "not implemented";
+  const packets = input
+    .replaceAll('\n\n', '\n')
+    .split('\n')
+    .map((packet) => JSON.parse(packet) as Packet);
+
+  const dividers = [[[2]], [[6]]];
+  packets.push(...dividers);
+
+  packets.sort(compare);
+
+  return (packets.indexOf(dividers[0]) + 1) * (packets.indexOf(dividers[1]) + 1);
+}
+
+export type Packet = Array<number[] | number | Packet> | number;
+
+export function compare(left: Packet, right: Packet): number {
+  if (typeof left === 'number' && typeof right === 'number') {
+    if (left < right) return -1;
+    if (left > right) return 1;
+    return 0;
+  }
+
+  if (Array.isArray(left) && Array.isArray(right)) {
+    let i = 0;
+    while (i < left.length && i < right.length) {
+      const result = compare(left[i], right[i]);
+      if (result !== 0) return result;
+      i++;
+    }
+    return compare(left.length, right.length);
+  }
+
+  if (typeof left === 'number' && Array.isArray(right)) {
+    return compare([left], right);
+  }
+
+  if (Array.isArray(left) && typeof right === 'number') {
+    4;
+    return compare(left, [right]);
+  }
+
+  throw new Error('invalid input');
 }
--- test/13.test.ts
+++ test/13.test.ts
@@ -1,6 +1,28 @@
-import { part1, part2 } from '../src/2022/13';
+import { compare, part1, part2 } from '../src/2022/13';
 
-const data = ``;
+const data = `[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]`;
 
 describe('Day 13 Tests', () => {
   it('should exist', () => {
@@ -8,11 +30,51 @@ describe('Day 13 Tests', () => {
     expect(part2).toBeDefined();
   });
 
-  // it('should solve part 1', () => {
-  //   expect(part1(data)).toBe();
-  // });
+  it('should solve part 1', () => {
+    expect(part1(data)).toBe(13);
+  });
 
-  // it('should solve part 2', () => {
-  //   expect(part2(data)).toBe();
-  // });
+  it('should compare pair 1', () => {
+    expect(compare([1, 1, 3, 1, 1], [1, 1, 5, 1, 1])).toBe(-1);
+  });
+
+  it('should compare pair 2', () => {
+    expect(compare([[1], [2, 3, 4]], [[1], 4])).toBe(-1);
+  });
+
+  it('should compare pair 3', () => {
+    expect(compare([9], [[8, 7, 6]])).toBe(1);
+  });
+
+  it('should compare pair 4', () => {
+    expect(compare([[4, 4], 4, 4], [[4, 4], 4, 4, 4])).toBe(-1);
+  });
+
+  it('should compare pair 5', () => {
+    expect(compare([7, 7, 7, 7], [7, 7, 7])).toBe(1);
+  });
+
+  it('should compare pair 6', () => {
+    expect(compare([], [3])).toBe(-1);
+  });
+
+  it('should compare pair 7', () => {
+    expect(compare([[[]]], [[]])).toBe(1);
+  });
+
+  it('should compare pair 8', () => {
+    expect(compare([1, [2, [3, [4, [5, 6, 7]]]], 8, 9], [1, [2, [3, [4, [5, 6, 0]]]], 8, 9])).toBe(1);
+  });
+
+  it('should return 1 if right side doesnt run out of items', () => {
+    expect(compare([[]], [[[]]])).toBe(-1);
+  });
+
+  it('should return 0 for same length lists with equal members', () => {
+    expect(compare([4, 4, 4], [4, 4, 4])).toBe(0);
+  });
+
+  it('should solve part 2', () => {
+    expect(part2(data)).toBe(140);
+  });
 });
